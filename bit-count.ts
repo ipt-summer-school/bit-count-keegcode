90bce64a9c918198801f47d7441fb81281144838
Danila <ukrobzoru@gmail.com>
2019-08-20 00:08:17 +0300

    fixed a small bug

--- bit-count.ts
+++ bit-count.ts
@@ -5,7 +5,7 @@ function bitCount(a: number): number {
     if(a < -(2**31) || a > 2**31 - 1) {
         throw new Error('Only 32-bits numbers allowed!')
     } else {
-       const binary: string = fromDecimalToBinary(-a, 32)
+       const binary: string = fromDecimalToBinary(a < 0 ? -a : a, 32)
        if(a < 0) {
            const negative: string = toAdditionalCode(binary);
            return ArrayToString(negative.split('').map((value: string) => value === "1" ? "1" : "")).length
