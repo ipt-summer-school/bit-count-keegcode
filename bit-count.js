a65430c5114d727ddae490535abd468bcbdc7a4a
Danila <ukrobzoru@gmail.com>
2019-08-20 00:04:52 +0300

    Cleaned testing stuff and encapsulated code

--- bit-count.js
+++ bit-count.js
@@ -1,58 +1,20 @@
-function XOR(a, b) {
-    return (!a && b) || (a && !b) ? 1 : 0;
-}
-function AND(a, b) {
-    return a && b ? 1 : 0;
-}
-function OR(a, b) {
-    return a || b ? 1 : 0;
-}
-function fullAdder(a, b) {
-    var register = [];
-    var Cinput = 0;
-    for (var i = Math.max(a.length, b.length); i >= 0; --i) {
-        var Sum = XOR(XOR(+a[i], +b[i]), Cinput);
-        var Cout = OR(AND(+a[i], +b[i]), AND(XOR(+a[i], +b[i]), Cinput));
-        Cinput = Cout;
-        register.push(Sum.toString());
-    }
-    return ArrayToString(register.reverse());
-}
-function fromDecimalToBinary(num, bits) {
-    var char = '';
-    var number = num;
-    var power = Math.floor(Math.log(num) / Math.log(2));
-    for (var i = 0; i < bits; ++i) {
-        if (i > power) {
-            char += '0';
-        }
-        else {
-            char += number % 2 === 0 ? '0' : '1';
-            number = Math.floor(number / 2);
-        }
-    }
-    return ArrayToString(char.split('').reverse());
-}
-function toAdditionalCode(num) {
-    var one = fromDecimalToBinary(1, num.split('').length);
-    var negative = num.split('').map(function (value) { return value === "1" ? "0" : "1"; }).reduce(function (a, b) { return a + b; });
-    return ArrayToString(fullAdder(negative, one).split(''));
-}
-function ArrayToString(array) {
-    return array.reduce(function (a, b) { return a + b; });
-}
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var array_to_string_1 = __importDefault(require("./lib/array-to-string"));
+var binary_converters_1 = require("./lib/binary-converters");
 function bitCount(a) {
     if (a < -(Math.pow(2, 31)) || a > Math.pow(2, 31) - 1) {
         throw new Error('Only 32-bits numbers allowed!');
     }
     else {
-        var binary = fromDecimalToBinary(-a, 32);
+        var binary = binary_converters_1.fromDecimalToBinary(-a, 32);
         if (a < 0) {
-            var negative = toAdditionalCode(binary);
-            console.log(binary, negative);
-            return ArrayToString(negative.split('').map(function (value) { return value === "1" ? "1" : ""; })).length;
+            var negative = binary_converters_1.toAdditionalCode(binary);
+            return array_to_string_1.default(negative.split('').map(function (value) { return value === "1" ? "1" : ""; })).length;
         }
-        return ArrayToString(binary.split('').map(function (value) { return value === "1" ? "1" : ""; })).length;
+        return array_to_string_1.default(binary.split('').map(function (value) { return value === "1" ? "1" : ""; })).length;
     }
 }
-console.log(bitCount(-10));
--- bit-count.ts
+++ bit-count.ts
@@ -1,65 +1,15 @@
-function XOR(a: number, b: number): 0 | 1 {
-    return (!a && b) || (a && !b) ? 1 : 0;
-}
+import ArrayToString from './lib/array-to-string';
+import { fromDecimalToBinary, toAdditionalCode } from './lib/binary-converters';
 
-function AND(a: number, b: number): 0 | 1 {
-    return a && b ? 1 : 0;
-}
-
-function OR(a: number, b: number): 0 | 1 {
-    return a || b ? 1 : 0;
-}
-
-function fullAdder(a: string, b: string): string {
-    let register: string[] = [];
-    let Cinput = 0;
-    for(let i = Math.max(a.length, b.length); i >= 0 ; --i) {
-        let Sum:  0 | 1 = XOR(XOR(+a[i], +b[i]), Cinput);
-        let Cout:  0 | 1 = OR(AND(+a[i], +b[i]), AND(XOR(+a[i], +b[i]), Cinput));
-        Cinput = Cout;
-        register.push(Sum.toString())
-    }
-
-    return ArrayToString(register.reverse())
-}
-
-function fromDecimalToBinary(num: number, bits: number): string {
-    let char: string = '';
-    let number = num;
-    const power: number = Math.floor(Math.log(num) / Math.log(2));
-    for(let i: number = 0; i < bits; ++i) {
-        if(i > power) {
-            char += '0';
-        } else {
-            char += number % 2 === 0 ? '0' : '1';
-            number = Math.floor(number/2) 
-        }
-    }
-    return ArrayToString(char.split('').reverse())
-}
-
-function toAdditionalCode(num: string) {
-    const one: string = fromDecimalToBinary(1, num.split('').length);
-    const negative = num.split('').map((value: string) => value === "1" ? "0" : "1").reduce((a: string, b: string) => a + b)
-    return ArrayToString(fullAdder(negative, one).split(''))
-}
-
-function ArrayToString(array: string[]): string {
-    return array.reduce((a: string, b: string) => a + b)
-}
-
-function bitCount(a: number) {
+function bitCount(a: number): number {
     if(a < -(2**31) || a > 2**31 - 1) {
         throw new Error('Only 32-bits numbers allowed!')
     } else {
-       const binary = fromDecimalToBinary(-a, 32)
+       const binary: string = fromDecimalToBinary(-a, 32)
        if(a < 0) {
-           const negative = toAdditionalCode(binary);
-           console.log(binary, negative)
+           const negative: string = toAdditionalCode(binary);
            return ArrayToString(negative.split('').map((value: string) => value === "1" ? "1" : "")).length
        }
        return ArrayToString(binary.split('').map((value: string) => value === "1" ? "1" : "")).length
     }
 }
-
-console.log(bitCount(-10))
